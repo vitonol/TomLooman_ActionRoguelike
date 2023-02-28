3a600532a8690c531c5e1d5ba99acca73551a0e9
Vitalii Voronkin <viton90@gmail.com>
2023-02-27 21:42:33 -0500

    Added more materials
    - Improved Health bar

---
+++ Source/ActionRoguelike/STargetDummy.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+#include "Components/StaticMeshComponent.h"
+#include "SAttributeComponent.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	RootComponent = MeshComp;
+
+	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+	// Trigger when health is changed (damage / healing)
+	AttributeComp->OnHealthChanged.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta)
+{
+	if (Delta < 0.f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds); // NO need to check for null because we created this component in the constructor
+	}
+}
+
+
